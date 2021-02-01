8b1e6c13d8ec98b199befffe06ac44f411fabd63
Iwan Skworcow <drina@mi-e1.msk.21-school.ru>
2021-02-01 18:50:47 +0300

    parse method editing

--- Request.hpp
+++ Request.hpp
@@ -2,25 +2,34 @@
 #define REQUEST_HPP
 #include <iostream>
 #include <map>
+#include <vector>
 
 class Request {
 private:
+	bool firstLine;
 	std::string method;
 	std::string path;
 	std::string htmlPage;
 	std::map<std::string, std::string> headers;
+
+	std::vector<std::string> split(const std::string& str, const std::string& delimeter);
+	void parseFirstLine(std::vector<std::string> const &arr);
+
 public:
 	Request();
 	virtual ~Request();
 
-	void parse(std::string const &line) const;
-	void addElemInMap(std::string &key, std::string &value);
-
 	void setPath(const std::string &path);
 	void setHtmlPage(const std::string &htmlPage);
+	void setMethod(const std::string &method);
 
 	const std::string &getPath() const;
+	const std::string &getMethod() const;
 	const std::string &getHtmlPage() const;
+
+	void parse(std::string const &line);
+	void addElemInMap(std::string &key, std::string &value);
+
 };
 
 #endif //REQUEST_HPP
